d07888a711eb6424c95e9500a9eb1a447553e9f2
Damien <damien.bouchabou@gmail.com>
2021-06-05 17:42:30 +0200

    Clean Ordonez dataset classes

--- SmartHomeHARLib/datasets/aras/oaras_house_A_dataset.py
+++ SmartHomeHARLib/datasets/aras/oaras_house_A_dataset.py
@@ -2,25 +2,23 @@
 # !/usr/bin/env python3
 
 import os
-import sys
-import pandas as pd
-import re
+
 
 from SmartHomeHARLib.datasets.ordonez import Ordonez
 from SmartHomeHARLib.utils.data_segmentation import *
 
 
-class HouseB(Ordonez):
+class HouseA(Ordonez):
 
     def __init__(self, path_to_dataset=None):
 
         # If no specific path to dataset is given, use the default one
         if path_to_dataset == None:
             current_file_directory = os.path.dirname(__file__)
-            path_to_dataset = current_file_directory+"/../original_datasets/ORDONEZ/house_B/OrdonezB"
+            path_to_dataset = current_file_directory+"/../original_datasets/ORDONEZ/house_A/OrdonezA"
             path_to_dataset = os.path.normpath(path_to_dataset)
 
-        super().__init__("HOUSE_B", path_to_dataset)
+        super().__init__("HOUSE_A", path_to_dataset)
 
             
 
